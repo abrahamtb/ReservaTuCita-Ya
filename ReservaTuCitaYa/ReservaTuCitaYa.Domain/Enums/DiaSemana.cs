c46c54d7b0a4c65d365744954320ffb5e20dd5d9
Diego <epy@gmail.com>
2026-07-27 20:36:35 -0500

    feat: crear BaseEntity y enumeraciones del dominio

---
+++ ReservaTuCitaYa/ReservaTuCitaYa.Domain/Enums/DiaSemana.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReservaTuCitaYa.Domain.Enums
+{
+    public enum DiaSemana
+    {
+        NoDefinido = 0,
+        Lunes = 1,
+        Martes = 2,
+        Miercoles = 3,
+        Jueves = 4,
+        Viernes = 5,
+        Sabado = 6,
+        Domingo = 7
+    }
+}
---
+++ ReservaTuCitaYa/ReservaTuCitaYa.UnitTests/Domain/EnumsTests.cs
@@ -0,0 +1,28 @@
+﻿using ReservaTuCitaYa.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReservaTuCitaYa.UnitTests.Domain
+{
+    public class EnumsTests
+    {
+        [Theory]
+        [InlineData(typeof(TipoDocumento))]
+        [InlineData(typeof(ModalidadServicio))]
+        [InlineData(typeof(EstadoReserva))]
+        [InlineData(typeof(EstadoPago))]
+        [InlineData(typeof(EstadoRecurso))]
+        [InlineData(typeof(DiaSemana))]
+        [InlineData(typeof(TipoBloqueo))]
+        public void TodosLosEnums_DebenTenerValorCeroDefinido(Type tipoEnum)
+        {
+            var valores = Enum.GetValues(tipoEnum);
+            var primerValor = (int)valores.GetValue(0)!;
+
+            Assert.Equal(0, primerValor);
+        }
+    }
+}
